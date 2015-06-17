e30371c39ae19aba46a6f1c1a8c14d92705ecbf0
QAston <qaston@gmail.com>
2015-06-17 22:17:53 +0200

    Initial project structure

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.codingdeficiency/urdmi "0.1.0-SNAPSHOT"
+  :description "Universal Relational Data Mining Interface"
+  :url "https://github.com/QAston/URDMI"
+  :license {:name "GNU General Public License, version 2"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [com.igormaznitsa/prologparser "1.3.2"]
+                 [clj-antlr "0.2.2"]
+                 [fx-clj "0.2.0-alpha1"]])
