8cb4618a2cc30fcfb5b63e7c2bf1c24b49407258
QAston <qaston@gmail.com>
2015-06-19 00:19:44 +0200

    Start switching parser to jiprolog.

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
                  [clj-antlr "0.2.2"]
                  [fx-clj "0.2.0-alpha1"]
                  [com.stuartsierra/component "0.2.3"]
+                 [org.apache.directory.studio/org.apache.commons.io "2.4"]
                  ]
   :profiles {:dev {:dependencies [[midje "1.7.0-beta1"]]}}
   :plugins [[lein-midje "3.0.0"]]
-  :java-source-paths ["deps/jiprolog/src"])
+  :java-source-paths ["deps/jiprolog/src"]
+  :javac-options ["-target" "1.8" "-source" "1.8"])
--- src/urdmi/prolog.clj
+++ src/urdmi/prolog.clj
@@ -1,15 +1,30 @@
 (ns urdmi.prolog
-  (:import (com.igormaznitsa.prologparser PrologParser PrologCharDataSource)
-           (java.io Reader)))
+  (:import (com.ugos.jiprolog.engine OperatorManager PrologParser JIPEngine)
+           (java.io Reader)
+           (org.apache.commons.io.input ReaderInputStream)
+           (com.ugos.io PushbackLineNumberInputStream)
+           (java.nio.charset Charset)))
 
 (defn ^:private prolog-expr-seq-impl [^PrologParser parser]
-  (when-let [next (.nextSentence parser)]
+  (when-let [next (.parseNext parser)]
     (cons next (lazy-seq (prolog-expr-seq-impl parser)))))
 
-(defn prolog-sentence-seq
+#_(defn prolog-sentence-seq
   "Lazily reads prolog expr from file, one at a time."
   ([^Reader rdr]
    (let [parser (PrologParser. nil)
          input-adapter (PrologCharDataSource. rdr)]
      (when-let [next (.nextSentence parser input-adapter)]
+       (cons next (lazy-seq (prolog-expr-seq-impl parser)))))))
+
+(defn- create-parser[^Reader rdr]
+  (PrologParser. (PushbackLineNumberInputStream. (ReaderInputStream. rdr (Charset/forName "US-ASCII")))
+                 (OperatorManager.)
+                 (JIPEngine.)
+                 "Input file"))
+(defn prolog-sentence-seq
+  "Lazily reads prolog expr from file, one at a time."
+  ([^Reader rdr]
+   (let [parser (create-parser rdr)]
+     (when-let [next (.parseNext parser)]
        (cons next (lazy-seq (prolog-expr-seq-impl parser)))))))
