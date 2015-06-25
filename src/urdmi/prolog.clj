7e4a7e5300c57c5b76e6c90ac81038bd1a8e5144
QAston <qaston@gmail.com>
2015-06-25 11:40:28 +0200

    Implement own ast format.
    Start work on prolog prettyprinter.

--- src/urdmi/prolog.clj
+++ src/urdmi/prolog.clj
@@ -1,5 +1,5 @@
 (ns urdmi.prolog
-  (:import (com.ugos.jiprolog.engine OperatorManager PrologParser JIPEngine ParserReader JIPDebugger PrologObject)
+  (:import (com.ugos.jiprolog.engine OperatorManager PrologParser JIPEngine ParserReader JIPDebugger PrologObject ConsCell Functor List Clause Atom Expression Variable)
            (java.io Reader)
            (org.apache.commons.io.input ReaderInputStream)
            (com.ugos.io PushbackLineNumberInputStream)
@@ -8,9 +8,11 @@
 
 (set! JIPDebugger/debug true)
 
+(declare to-ast)
+
 (defn ^:private prolog-expr-seq-impl [^PrologParser parser]
   (when-let [next (.parseNext parser)]
-    (cons next (lazy-seq (prolog-expr-seq-impl parser)))))
+    (cons (to-ast next) (lazy-seq (prolog-expr-seq-impl parser)))))
 
 (defrecord Operator [^int preference ^String type ^String name])
 
@@ -22,7 +24,7 @@
     (doseq [^Operator op operators]
       (.put op-manager (.preference op) (.type op) (.name op)))
     (->ParserContext jip-engine op-manager
-      )))
+                     )))
 
 (defn create-parser ^PrologParser [^ParserContext context, ^Reader rdr]
   (PrologParser. (ParserReader. (PushbackLineNumberInputStream. (ReaderInputStream. rdr (Charset/forName "US-ASCII"))))
@@ -51,4 +53,256 @@
   ([^ParserContext context ^Reader rdr]
    (let [parser (create-parser context rdr)]
      (when-let [next (.parseNext parser)]
-       (cons next (lazy-seq (prolog-expr-seq-impl parser)))))))
+       (cons (to-ast next) (lazy-seq (prolog-expr-seq-impl parser)))))))
+
+(def ast-cell :ast-cell)
+(def ast-object :ast-object)
+(def ast-atom :ast-atom)
+(def ast-expression :ast-expression)
+(def ast-variable :ast-variable)
+(def ast-functor :ast-functor)
+(def ast-list :ast-list)
+(def ast-clause :ast-clause)
+
+(def ast
+  (let [parent-to-child [
+                         [ast-cell ast-functor]
+                         [ast-cell ast-list]
+                         [ast-cell ast-clause]
+                         [ast-object ast-cell]
+                         [ast-object ast-atom]
+                         [ast-object ast-expression]
+                         [ast-object ast-variable]]]
+
+    (loop [h (make-hierarchy) mapping parent-to-child]
+      (if-not (empty? mapping)
+        (recur (derive h (second (first mapping)) (first (first mapping)))
+               (rest mapping))
+        h))
+    ))
+
+(defprotocol PAstConvertible
+  (to-ast [^PrologObject obj])
+  )
+
+(defn- with-file-metadata [^PrologObject obj m]
+  (with-meta m
+             {:line         (.getLine obj)
+              :column       (.getColumn obj)
+              :get-position (.getPosition obj)}))
+
+(extend-type Functor
+  PAstConvertible
+  (to-ast [^Functor obj]
+    (with-file-metadata obj
+                        {:type ast-functor
+                         :head (assoc (to-ast (.getHead obj)) :name (.getFriendlyName obj))
+                         :tail (to-ast (.getTail obj))})))
+
+(defn get-functor-name
+  "returns name for given functor ast node"
+  [functor]
+  {:pre [(= (:type functor) ast-functor)]
+   :post [(not= % nil)]}
+  (:name (:head functor)))
+
+(defn get-functor-arity
+  "returns arity for given functor ast node"
+  [functor]
+  {:pre [(= (:type functor) ast-functor)]
+   :post [(not= % nil)]}
+  (loop [head (:tail functor) i 0]
+    (if head
+      (recur (:tail head) (inc i))
+      i)))
+
+(extend-type List
+  PAstConvertible
+  (to-ast [^List obj]
+    (with-file-metadata obj
+                        {:type ast-list
+                         :head (to-ast (.getHead obj))
+                         :tail (to-ast (.getTail obj))})))
+
+(extend-type Clause
+  PAstConvertible
+  (to-ast [^Clause obj]
+    (with-file-metadata obj
+                        {:type ast-clause
+                         :head (to-ast (.getHead obj))
+                         :tail (to-ast (.getTail obj))}
+                        )))
+
+(extend-type ConsCell
+  PAstConvertible
+  (to-ast [^Clause obj]
+    (with-file-metadata obj
+                        {:type ast-cell
+                         :head (to-ast (.getHead obj))
+                         :tail (to-ast (.getTail obj))}
+                        )))
+
+(extend-type Atom
+  PAstConvertible
+  (to-ast [^Atom obj]
+    (with-file-metadata obj
+                        {:type ast-atom
+                         :name (.getName obj)}
+                        )))
+
+(extend-type Expression
+  PAstConvertible
+  (to-ast [^Expression obj]
+    (with-file-metadata obj
+                        {:type  ast-expression
+                         :value (if (.isInteger obj) (int (.getValue obj))
+                                                     (.getValue obj))}
+                        )))
+
+(extend-type Variable
+  PAstConvertible
+  (to-ast [^Variable obj]
+    (with-file-metadata obj
+                        {:type ast-variable
+                         :name (.getName obj)}
+                        )))
+
+(extend-type nil
+  PAstConvertible
+  (to-ast [obj]
+    nil))
+
+
+(defmulti pretty-print "Pretty prints a prolog ast node into builder."
+          (fn [node op-manager ^StringBuilder builder] (:type node)))
+
+(defn- pretty-print-cons [obj op-manager ^StringBuilder builder]
+  (loop [head (:head obj)]
+    (when head
+      (pretty-print head, op-manager, builder)
+      (let [tail (:tail obj)]
+        (when (:head tail)
+          (.append builder ","))
+        (recur tail)))))
+
+(defn- pretty-print-operator [functor ^com.ugos.jiprolog.engine.Operator op op-manager ^StringBuilder builder]
+  (let [arity (get-functor-arity functor)
+        args (:tail functor)]
+    (cond
+      (and (== arity 1) (.getPrefix op)) (do
+                                           (pretty-print (:head functor) op-manager builder)
+                                           (.append builder " ")
+                                           (pretty-print (:head args) op-manager builder)
+                                           )
+      (and (== arity 1) (.getPostfix op)) (do
+                                            (pretty-print (:head args) op-manager builder)
+                                            (.append builder " ")
+                                            (pretty-print (:head functor) op-manager builder)
+                                            )
+      :else (do
+              (pretty-print (:head args) op-manager builder)
+              (.append builder " ")
+              (pretty-print (:head functor) op-manager builder)
+              (.append builder " ")
+              (let [next-arg (:tail args)
+                    type (:type next-arg)]
+                (if (or (isa? ast type ast-list)
+                        (isa? ast type ast-functor)
+                        (not (isa? ast type ast-cell)))
+                  (pretty-print next-arg op-manager builder)
+                  (pretty-print (:head next-arg) op-manager builder)))
+              ))))
+
+(defn is-operator
+  "returns true when functor is found to be an operator in this ast node"
+  [functor op-manager]
+  (let [name (get-functor-name functor)]
+    (when (.contains op-manager name)
+      (let [^com.ugos.jiprolog.engine.Operator op (.get op-manager name)
+            arity (get-functor-arity functor)]
+        (when (or (and (== arity 1)
+                       (or (.getPrefix op) (.getPostfix op)))
+                  (and (== arity 2) (.getInfix op)))
+          op
+          )))))
+
+
+(defn- pretty-print-params [obj op-manager ^StringBuilder builder]
+  (loop [head (:head obj)]
+    (when head
+      (pretty-print head, op-manager, builder)
+      (let [tail (:tail obj)]
+        (if (isa? ast (:type obj) ast-cell)
+          (do
+            (when (:head tail)
+              (.append builder ","))
+            (recur tail))
+          (do
+            (.append builder "|")
+            (pretty-print tail op-manager builder)))
+        ))))
+
+(defmethod pretty-print ast-clause [obj op-manager ^StringBuilder builder]
+  (pretty-print (:head obj) op-manager builder)
+  (when-let [tail (:tail obj)]
+    (.append builder ":-")
+    (pretty-print (:head tail)))                            ;print more than just head?
+  (.append builder ".")
+  )
+
+(defmethod pretty-print ast-functor [functor op-manager ^StringBuilder builder]
+  (if-not (:tail functor)
+    (pretty-print (:head functor))
+    (if-let [op (is-operator functor op-manager)]
+      (pretty-print-operator functor op op-manager builder)
+      (let [arity (get-functor-arity functor)
+            params (:tail functor)]
+
+        (if (and (== arity 1)
+                 (= (get-functor-name functor) "{")
+                 (= (get-functor-name params) "}"))
+          ; special case - array
+          (if (== 0 (get-functor-arity (:tail params)))
+            (.append builder "{}")
+            (do
+              (.append builder "{}(")
+              (pretty-print-params (:tail params) op-manager builder)
+              (.append builder ")")
+              ))
+
+          (do
+            (pretty-print (:head functor) op-manager builder)
+            (.append builder "(")
+            (pretty-print-params params op-manager builder)
+            (.append builder ")")))
+
+        ))))
+
+(defmethod pretty-print ast-list [obj op-manager ^StringBuilder builder]
+  (.append builder "[")
+  (pretty-print-cons obj op-manager builder)
+  (.append builder "]")
+  )
+
+(defmethod pretty-print ast-atom [obj _ ^StringBuilder builder]
+  (.append builder (:name obj))
+  )
+
+(defmethod pretty-print ast-expression [obj _ ^StringBuilder builder]
+  (.append builder (:value obj))
+  )
+
+(defmethod pretty-print ast-variable [obj _ ^StringBuilder builder]
+  (.append builder (:name obj))
+  )
+
+(defmethod pretty-print nil [_ _ ^StringBuilder _]
+  )
+
+(defn pretty-print-sentences
+  "Pretty-prints a a seq of prolog ast nodes. Assumes these are in order."
+  [^ParserContext context, ^ISeq prolog-sentences]
+  ;todo: handle seqs
+  (let [builder (StringBuilder.)]
+    (pretty-print (first prolog-sentences) (:op-manager context) builder)
+    (.toString builder)))
--- test/urdmi/prolog_test.clj
+++ test/urdmi/prolog_test.clj
@@ -4,7 +4,44 @@
            (com.ugos.jiprolog.engine PrologObject))
   (:require
     [urdmi.prolog :as prolog]
-    [clojure.java.io :as io]))
+    [clojure.java.io :as io]
+    [clojure.string :as str]))
+
+(fact "prolog ast hierarchy is defined"
+      (isa? prolog/ast prolog/ast-cell prolog/ast-object) => true
+      (isa? prolog/ast prolog/ast-functor prolog/ast-expression) => false
+      (isa? prolog/ast prolog/ast-list prolog/ast-cell) => true)
+
+(defn parse-string [context str]
+  (prolog/prolog-sentence-seq context (StringReader. str)))
+
+(fact "prolog parser produces ast nodes"
+      (let [parser-context (prolog/ace-parser-context)]
+        (parse-string parser-context "hello :- world.") => (list {:head {:name ":-", :type :ast-atom},
+                                                                  :tail
+                                                                        {:head {:name "hello", :type :ast-atom},
+                                                                         :tail
+                                                                               {:head {:name "world", :type :ast-atom}, :tail nil, :type :ast-cell},
+                                                                         :type :ast-cell},
+                                                                  :type :ast-functor})
+        (parse-string parser-context "p(X).") => (list {:head {:name "p", :type :ast-atom},
+                                                        :tail
+                                                              {:head {:name "X", :type :ast-variable},
+                                                               :tail nil,
+                                                               :type :ast-cell},
+                                                        :type :ast-functor})
+        (parse-string parser-context "q(+-\\Y).") => (list {:head {:name "q", :type :ast-atom},
+                                                            :tail
+                                                                  {:head
+                                                                         {:head {:name "+-\\", :type :ast-atom},
+                                                                          :tail
+                                                                                {:head {:name "Y", :type :ast-variable},
+                                                                                 :tail nil,
+                                                                                 :type :ast-cell},
+                                                                          :type :ast-functor},
+                                                                   :tail nil,
+                                                                   :type :ast-cell},
+                                                            :type :ast-functor})))
 
 (fact "prolog expr seq returns a seq of ast nodes"
       (let [parser-context (prolog/parser-context nil)
@@ -15,7 +52,7 @@
         (count single-sentence) => 1
         (count two-sentences) => 2
         (count empty) => 0
-        (instance? PrologObject (first two-sentences)) => true
+        (isa? prolog/ast (:type (first two-sentences)) prolog/ast-object) => true
         )
       )
 
@@ -31,14 +68,14 @@
 (fact "parser can parse example ace prolog files"
       (let [files (get-prolog-files-for-tests "dev-resources/ace_tilde")
             parser-context (prolog/ace-parser-context)]
-         (doseq [file files]
-           (fact file
-                 (try (count (prolog/prolog-sentence-seq parser-context
-                           (io/make-reader
-                             (io/resource file) {})))
-                      (catch Exception e
-                        (throw (Exception. (str "Couldn't parse " file) e)))) => anything)
-           )))
+        (doseq [file files]
+          (fact file
+                (try (count (prolog/prolog-sentence-seq parser-context
+                                                        (io/make-reader
+                                                          (io/resource file) {})))
+                     (catch Exception e
+                       (throw (Exception. (str "Couldn't parse " file) e)))) => anything)
+          )))
 
 (fact "parser can parse example aleph prolog files"
       (let [files (get-prolog-files-for-tests "dev-resources/aleph_default")
@@ -46,8 +83,27 @@
         (doseq [file files]
           (fact file
                 (try (count (prolog/prolog-sentence-seq parser-context
-                              (io/make-reader
-                                (io/resource file) {})))
+                                                        (io/make-reader
+                                                          (io/resource file) {})))
                      (catch Exception e
                        (throw (Exception. (str "Couldn't parse " file) e)))) => anything)
           )))
+
+
+(fact "pretty-print prints the same thing it got"
+      (let [parser-context (prolog/ace-parser-context)
+            test-sentences ["hello :- world."
+                            "p(X)."
+                            "p(+X)."
+                            "q(+-Y)."
+                            "q(-Y)."
+                            "q(\\Y)."
+                            "q(+\\Y)."
+                            "q(-\\Y)."
+                            "q(+-\\Y)."
+                            ]]
+        (doseq [sentence test-sentences]
+          (str/join (filter #(not= % \space)
+                            (prolog/pretty-print-sentences
+                              parser-context
+                              (parse-string parser-context sentence)))) => (str/join (filter #(not= % \space) (butlast sentence))))))
