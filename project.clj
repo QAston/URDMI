ce343fd3621aa9976cc1df884d234bf81c0f57d9
QAston <qaston@gmail.com>
2015-07-19 12:43:18 +0200

    Add jsr223 impl for clojure for embeding clojure in fxml.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [com.stuartsierra/component "0.2.3"]
                  [org.apache.directory.studio/org.apache.commons.io "2.4"]
                  [potemkin "0.3.13"]
-                 [me.raynes/fs "1.4.6"]
+                 [me.raynes/fs "1.4.6" :exclusions [potemkin]]
+                 [org.clojars.ato/clojure-jsr223 "1.5.1" :exclusions [org.clojure/clojure]]
                  ]
   :profiles {:dev {:dependencies [[midje "1.7.0-beta1"]]}}
   :plugins [[lein-midje "3.0.0"]]
