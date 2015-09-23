7d60ac7afed9949990144f834fa6f28bf6cccc35
QAston <qaston@gmail.com>
2015-09-23 14:49:16 +0200

    Implement a directory select widget.

--- src/urdmi/gui.clj
+++ src/urdmi/gui.clj
@@ -1,18 +1,32 @@
 (ns urdmi.gui
   (:require [clojure.core.async :refer [chan go <! >!]]
             [clojure.java.io :as io]
-            [fx-clj.core :as fx])
+            [fx-clj.core :as fx]
+            [urdmi.core :as core]
+            [me.raynes.fs :as fs])
   (:import (javafx.collections ObservableList FXCollections)
            (java.util Collection)
-           (javafx.beans.value ObservableValue ChangeListener)
+           (javafx.beans.value ObservableValue ChangeListener WritableValue)
            (org.controlsfx.validation ValidationSupport Validator Severity)
            (org.controlsfx.validation.decoration ValidationDecoration)
-           (javafx.scene.control Control TableCell Labeled)
+           (javafx.scene.control Control TableCell Labeled TextField)
            (java.util.function Predicate)
            (clojure.lang IFn)
            (javafx.util Callback)
            (org.controlsfx.tools ValueExtractor)
-           (javafx.scene.input KeyCodeCombination KeyCode)))
+           (javafx.scene.input KeyCodeCombination KeyCode)
+           (javafx.event EventHandler)
+           (javafx.beans.property Property)
+           (java.util Optional)
+           (javafx.scene.control ToggleButton)
+           (javafx.geometry Insets)
+           (java.io File)
+           (javafx.scene.layout HBox Priority)
+           (javafx.stage DirectoryChooser)
+           (org.controlsfx.validation ValidationSupport)
+           (javafx.beans.property SimpleStringProperty)
+           (org.controlsfx.validation.decoration StyleClassValidationDecoration)
+           (org.controlsfx.control SegmentedButton)))
 
 (defn load-fxml [filename]
   (let [loader (new javafx.fxml.FXMLLoader (io/resource filename))]
@@ -106,5 +120,87 @@
 (defn ctrl-key-accelerator[^KeyCode code]
   (KeyCodeCombination. code (into-array (list KeyCodeCombination/SHORTCUT_DOWN))))
 
+(defn on-text-field-confirmed
+  "textfield-fn is called when focus lost, or field confirmed using [enter]
+  textfield-fn is (fn [text-field])"
+  [^TextField text-field textfield-fn]
+  (on-changed (.focusedProperty text-field)
+                  (fn [observable old new]
+                    (when-not new
+                      (textfield-fn text-field))))
+  (.setOnAction text-field (reify EventHandler
+                             (handle [this e]
+                               (textfield-fn text-field)))))
 
+(defn loose-bind [src-property ^WritableValue target-property]
+  (on-changed src-property
+                  (fn [obs old new]
+                    (when (not= old new)
+                      (.setValue target-property new)))))
+
+(defn str-to-file [^File base-dir s]
+  (let [^File file (io/file s)]
+    (if (fs/absolute? file)
+      file
+      (fs/normalized (fs/file base-dir file)))))
+
+(defn make-fs-select-widget
+  "browsing-fn is (fn [e] show-dialog)"
+  [^File base-dir file-str text-field browsing-fn]
+  (let [relative-btn (ToggleButton. "Relative")
+        absolute-btn (ToggleButton. "Absolute")
+        absolute-relative-toggle (SegmentedButton.
+                                   (observable-list (list relative-btn
+                                                              absolute-btn)))
+        ]
+    (on-text-field-confirmed text-field (fn [text-field]
+                                              (.setValue file-str (.getText text-field))))
+    (loose-bind file-str (.textProperty text-field))
+    (on-changed (.. absolute-relative-toggle
+                        getToggleGroup
+                        selectedToggleProperty)
+                    (fn [obs old type]
+                      (let [file ^File (io/file (.getValue file-str))
+                            new-file (if (= type relative-btn)
+                                       (if (fs/absolute? file)
+                                         (core/relativize-path base-dir file)
+                                         file)
+                                       (str-to-file base-dir file))]
+                        (.setValue file-str (str new-file))
+                        )))
+    (on-changed file-str
+                    (fn [obs old new]
+                      (when (not= old new)
+                        (let [file (io/file new)]
+                          (if (fs/absolute? file)
+                            (.setSelected absolute-btn true)
+                            (.setSelected relative-btn true))))))
+    (doto (fx/h-box {:spacing 8}
+                    (fx/button {:text "Browse" :on-action browsing-fn})
+                    (doto text-field
+                      (HBox/setHgrow Priority/ALWAYS))
+                    absolute-relative-toggle))
+    ))
+
+(defn make-directory-select-widget [^File relative-to file-str-property description ^ValidationSupport validation]
+  (let [text-field (fx/text-field {})]
+    (validate-control validation text-field
+                          (fn [value]
+                            (let [file (str-to-file relative-to value)]
+                              (and (.exists file) (.isDirectory file))
+                              ))
+                          "You must select an existing directory.")
+    (make-fs-select-widget relative-to file-str-property text-field
+                           (fn [e]
+                             (let [file (.showDialog
+                                          (doto (DirectoryChooser.)
+                                            (.setInitialDirectory (str-to-file relative-to (.getValue file-str-property)))
+                                            (.setTitle description)
+                                            )
+                                          nil)]
+                               (when file
+                                 (.setValue file-str-property (str file)))
+                               )))))
+
+;(fx/sandbox #(make-directory-select-widget (fs/file ".") (SimpleStringProperty. "") "Select working directory dir" (validation-support (StyleClassValidationDecoration.))))
 
